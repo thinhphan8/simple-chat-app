cb77e285904c3718cb2a027ffbf79ddb28f4364c
SOMETHINGG\thinh <thinhphanvn8499@gmail.com>
2024-09-03 17:16:12 +0700

    Add message notification

--- src/components/ListMessages.tsx
+++ src/components/ListMessages.tsx
@@ -11,6 +11,7 @@ import { DeleteAlert, EditAlert } from "./MessageActions";
 export default function ListMessages() {
   const scrollRef = useRef() as React.MutableRefObject<HTMLDivElement>;
   const [userScrolled, setUserScrolled] = useState(false);
+  const [notification, setNotification] = useState(0);
   const {
     messages,
     addMessage,
@@ -44,6 +45,10 @@ export default function ListMessages() {
               addMessage(newMessage as IMessage);
             }
           }
+          const scrollContainer = scrollRef.current;
+          if (scrollContainer.scrollTop < scrollContainer.clientHeight - 10) {
+            setNotification((current) => current + 1);
+          }
         }
       )
       .on(
@@ -71,7 +76,7 @@ export default function ListMessages() {
 
   useEffect(() => {
     const scrollContainer = scrollRef.current;
-    if (scrollContainer) {
+    if (scrollContainer && !userScrolled) {
       scrollContainer.scrollTop = scrollContainer.scrollHeight;
     }
   }, [messages]);
@@ -82,41 +87,68 @@ export default function ListMessages() {
       const isScroll =
         scrollContainer.scrollTop < scrollContainer.clientHeight - 10;
       setUserScrolled(isScroll);
+      if (scrollContainer.scrollTop === scrollContainer.clientHeight) {
+        setNotification(0);
+      }
     }
   };
 
-  const handleScrollDown = () => {
+  const scrollDown = () => {
+    setNotification(0);
     scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-  }
+  };
 
   return (
-    <div
-      className="flex-1 flex flex-col p-5 h-full overflow-y-auto"
-      ref={scrollRef}
-      onScroll={handleOnScroll}
-    >
-      <div className="flex-1"></div>
-      <div className="space-y-7">
-        {messages.map((value, index) => {
-          return <Message key={index} message={value} />;
-        })}
+    <>
+      <div
+        className="flex-1 flex flex-col p-5 h-full overflow-y-auto"
+        ref={scrollRef}
+        onScroll={handleOnScroll}
+      >
+        <div className="flex-1"></div>
+        <div className="space-y-7">
+          {messages.map((value, index) => {
+            return <Message key={index} message={value} />;
+          })}
+        </div>
+        <DeleteAlert />
+        <EditAlert />
       </div>
       {userScrolled && (
-        <div className="absolute bottom-20 right-1/2">
-          <div
-            className="w-10 h-10 
-        bg-blue-500 rounded-full 
-        justify-center items-center 
-        flex mx-auto border cursor-pointer
-        hover:scale-110 transition-all"
-            onClick={handleScrollDown}
-          >
-            <ArrowDown />
-          </div>
+        <div className="absolute bottom-20 w-full">
+          {notification ? (
+            <div
+              className="w-40
+                        mx-auto 
+                        bg-indigo-500 
+                        p-1 
+                        text-center 
+                        rounded-md 
+                        cursor-pointer 
+                        hover:scale-110 
+                        transition-all"
+              onClick={scrollDown}
+            >
+              <h1>New {notification} message(s)</h1>
+            </div>
+          ) : (
+            <div
+              className="w-10 h-10 
+                        bg-blue-500 
+                        rounded-full 
+                        justify-center items-center 
+                        flex mx-auto 
+                        border 
+                        cursor-pointer
+                        hover:scale-110 
+                        transition-all"
+              onClick={scrollDown}
+            >
+              <ArrowDown />
+            </div>
+          )}
         </div>
       )}
-      <DeleteAlert />
-      <EditAlert />
-    </div>
+    </>
   );
 }
